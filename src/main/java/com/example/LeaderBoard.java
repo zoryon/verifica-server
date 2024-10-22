4426c2e72749d77e17f42182c5890c8d471ffbf0
lai2-24 <lai2-24@itismeucci.com>
2024-10-22 14:24:28 +0200

    non lo so, sto impazzendo

--- src/main/java/com/example/LeaderBoard.java
+++ src/main/java/com/example/LeaderBoard.java
@@ -1,5 +1,33 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class LeaderBoard {
-    
+    private ArrayList<ClientHandler> leaderBoard;
+
+    private static boolean isFinished = false;
+
+    public LeaderBoard() {
+        leaderBoard = new ArrayList<>();
+    }
+
+    public boolean addClient(ClientHandler client) {
+        return leaderBoard.add(client);
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public void setFinished(boolean isFinished) {
+        LeaderBoard.isFinished = isFinished;
+    }
+
+    public void printLeaderBoard() {
+        System.out.println("Classifica finale: ");
+
+        for (int i = 0; i < leaderBoard.size(); i++) {
+            System.out.println((i + 1) + ") " + leaderBoard.get(i).getId());
+        }
+    }
 }
